31d2994c9b6d96aea1735dd9d8989f04cdcc0e8c
Gary Menezes <gmensignup@gmail.com>
2011-11-12 16:25:53 -0500

    everything

---
+++ precomp.h
@@ -0,0 +1,18 @@
+
+#pragma once
+
+#include <vector>
+#include <ClanLib/core.h>
+#include <ClanLib/display.h>
+#include <ClanLib/gl.h>
+#include <ClanLib/gl1.h>
+#include <ClanLib/swrender.h>
+#include <ClanLib/gui.h>
+#include <ClanLib/sound.h>
+#include <ClanLib/vorbis.h>
+#include <ClanLib/mikmod.h>
+#include <ClanLib/network.h>
+#include <ClanLib/database.h>
+#include <ClanLib/sqlite.h>
+#include <ClanLib/regexp.h>
+#include <ClanLib/csslayout.h>
