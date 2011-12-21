036f578393cc2cd6ac3e9d3c88ad7395e2448dee
Gary Menezes <gmensignup@gmail.com>
2011-12-21 14:47:29 -0500

    moving on own buildings semi-fix

--- GameController.cpp
+++ GameController.cpp
@@ -70,7 +70,7 @@ Menu_Enum GameController::selectSquare(int row, int col, int current_player){
 			map->populateAttack();
 			map->setSelected(-1,-1);
 		}
-	}else if(map->highlighted[row][col] && (building == NULL || building->getPlayer()->number() != current_player)){
+	}else if(map->highlighted[row][col] && (building == NULL || (building->getPlayer()->number() != current_player || building->getType() != BASE))){
 		if(map->getRowSelected() >= 0){
 			unit = map->units[map->getRowSelected()][map->getColSelected()];
 			if(unit != NULL && unit->canMove()){
